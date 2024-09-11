a0f8daabd05c1cb02f32d5bc087a9163c2b39f2a
Liestro <andrey.istomin511@gmail.com>
2024-09-11 12:45:49 +0400

    add some handlers

---
+++ server.py
@@ -0,0 +1,12 @@
+from sanic import Sanic
+from sanic.response import text
+
+app = Sanic("MyHelloWorldApp")
+
+@app.get("/")
+async def hello_world(request):
+    return text("Hello, world.")
+
+@app.get("/foo")
+async def foo_handler(request):
+    return text("I sad foo!")
